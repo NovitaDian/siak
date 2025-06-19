2402fc0e0a4247b281db0ea763f28a0f246bf1d9
Novita Dian <novitadiansaf@gmail.com>
2025-06-19 22:01:57 +0700

    feat:gl acc edit

--- app/Http/Controllers/GLAccountController.php
+++ app/Http/Controllers/GLAccountController.php
@@ -48,7 +48,7 @@ class GLAccountController extends Controller
         $gl = Gl_Account::findOrFail($id);
 
         $request->validate([
-            'gl_code' => 'required|string|unique|max:50',
+            'gl_code' => 'required|string|max:50|unique:gl_accounts,gl_code,' . $id,
             'gl_name' => 'required|string|max:100',
 
         ]);
@@ -64,5 +64,63 @@ class GLAccountController extends Controller
         $gl->delete();
 
         return redirect()->route('adminsystem.master.glaccount.index')->with('success', 'Purchase Request berhasil dihapus.');
+    }
+    public function operator_index()
+    {
+        $gls = Gl_Account::all();
+        return view('operator.master.glaccount.index', compact('gls'));
+    }
+
+    public function operator_create()
+    {
+        return view('operator.master.glaccount.create');
+    }
+
+    public function operator_store(Request $request)
+    {
+
+        $request->validate([
+            'gl_code' => 'required|string|max:50|unique:gl_account,gl_code',
+            'gl_name' => 'required|string|max:100',
+        ]);
+
+
+        Gl_Account::create([
+            'gl_code' => $request->input('gl_code'),
+            'gl_name' => $request->input('gl_name'),
+        ]);
+
+        // Redirect back with a success message
+        return redirect()->route('operator.glaccount.index')->with('success', ' berhasil dibuat.');
+    }
+
+
+    public function operator_edit($id)
+    {
+        $gl = Gl_Account::findOrFail($id);
+        return view('operator.master.glaccount.edit', compact('gl'));
+    }
+
+    public function operator_update(Request $request, $id)
+    {
+        $gl = Gl_Account::findOrFail($id);
+
+        $request->validate([
+            'gl_code' => 'required|string|max:50|unique:gl_accounts,gl_code,' . $id,
+            'gl_name' => 'required|string|max:100',
+
+        ]);
+
+        $gl->update($request->all());
+
+        return redirect()->route('operator.master.glaccount.index')->with('success', 'GL Account berhasil diperbarui.');
+    }
+
+    public function operator_destroy($id)
+    {
+        $gl = Gl_Account::findOrFail($id);
+        $gl->delete();
+
+        return redirect()->route('operator.master.glaccount.index')->with('success', 'Purchase Request berhasil dihapus.');
     }
 }
