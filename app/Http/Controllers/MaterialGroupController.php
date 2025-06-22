bbc64fe3ded590c5cab2fbfbbe67e9e4ef15eb4b
Novita Dian <novitadiansaf@gmail.com>
2025-06-22 17:01:57 +0700

    mat group

--- app/Http/Controllers/MaterialGroupController.php
+++ app/Http/Controllers/MaterialGroupController.php
@@ -63,4 +63,47 @@ class MaterialGroupController extends Controller
         $material_groups = MaterialGroup::all();
         return view('operator.master.material_group.index', compact('material_groups'));
     }
+        public function operator_create()
+    {
+        return view('operator.master.material_group.create');
+    }
+
+    public function operator_store(Request $request)
+    {
+        $request->validate([
+            'material_group' => 'required|string'
+        ]);
+
+        MaterialGroup::create($request->all());
+
+        return redirect()->route('operator.material_group.index')->with('success', 'Purchase Request berhasil dibuat.');
+    }
+
+    public function operator_edit($id)
+    {
+        $material_group = MaterialGroup::findOrFail($id);
+        return view('operator.master.material_group.edit', compact('material_group'));
+    }
+
+    public function operator_update(Request $request, $id)
+    {
+        $material_group = MaterialGroup::findOrFail($id);
+
+        $request->validate([
+            'material_group' => 'required|string',
+        ]);
+
+        $material_group->update($request->all());
+
+        return redirect()->route('operator.material_group.index')->with('success', 'Purchase Request berhasil diperbarui.');
+    }
+
+    public function operator_destroy($id)
+    {
+        $material_group = MaterialGroup::findOrFail($id);
+        $material_group->delete();
+
+        return redirect()->route('operator.material_group.index')->with('success', 'Purchase Request berhasil dihapus.');
+    }
+
 }
